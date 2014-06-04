e2fcbf43c3242926e003730068714921b0566264
Sunny <Tianqing@SunnydeMacBook-Pro.local>
2014-06-04 23:50:19 +0800

    Initial Commit

---
+++ maze/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  maze
+//
+//  Created by Sunny on 14-6-4.
+//  Copyright (c) 2014年 Sunny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
